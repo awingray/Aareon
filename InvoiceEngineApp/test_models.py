b33b5d387936f2312b23241824aa36aea4747b7c
Awin <grayawin@gmail.com>
2021-04-18 22:09:18 +0700

    update

--- InvoiceEngineApp/test_models.py
+++ InvoiceEngineApp/test_models.py
@@ -22,6 +22,9 @@ class TenancyTest(TestCase):
         self.assertEqual(self.tenancy.number_of_contracts, 0)
         self.assertEqual(self.tenancy.last_invoice_number, 0)
 
+    def test_invoice_contracts(self):
+        pass
+
 
 class VATRateTest(TestCase):
     def setUp(self):
@@ -41,15 +44,6 @@ class ContractTypeTest(TestCase):
         self.assertEqual(ContractType.objects.all().count(), 1)
 
 
-class ContractTest(TestCase):
-    def setUp(self):
-        self.contract = baker.make(Contract)
-
-    def test_creation(self):
-        print(self.contract.get_details())
-        self.assertEqual(ContractType.objects.all().count(), 1)
-
-
 class BaseComponentTest(TestCase):
     def setUp(self):
         self.base_component = baker.make(BaseComponent)
@@ -57,3 +51,48 @@ class BaseComponentTest(TestCase):
     def test_creation(self):
         print(self.base_component.get_details())
         self.assertEqual(BaseComponent.objects.all().count(), 1)
+
+
+class ContractTest(TestCase):
+    def setUp(self):
+        self.contract = baker.make(Contract)
+
+    def test_creation(self):
+        print(self.contract.get_details())
+        self.assertEqual(Contract.objects.all().count(), 1)
+
+
+class ComponentTest(TestCase):
+    def setUp(self):
+        self.component = baker.make(Component)
+
+    def test_creation(self):
+        print(self.component.get_details())
+        self.assertEqual(Component.objects.all().count(), 1)
+
+
+class ContractPersonTest(TestCase):
+    def setUp(self):
+        self.contract_person = baker.make(ContractPerson)
+
+    def test_creation(self):
+        print(self.contract_person.get_details())
+        self.assertEqual(contract_person.objects.all().count(), 1)
+
+
+class InvoiceTest(TestCase):
+    def setUp(self):
+        self.invoice = baker.make(Invoice)
+
+    def test_creation(self):
+        print(self.invoice.get_details())
+        self.assertEqual(Invoice.objects.all().count(), 1)
+
+
+class InvoiceLineTest(TestCase):
+    def setUp(self):
+        self.invoice_line = baker.make(InvoiceLine)
+
+    def test_creation(self):
+        print(self.invoice_line.get_details())
+        self.assertEqual(InvoiceLine.objects.all().count(), 1)
